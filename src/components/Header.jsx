8f9330c1bf3eef4e5af647bfadbbe0655b92ec05
shamimfstack <shamimfstack@gmail.com>
2024-04-01 18:00:37 +0600

    header added

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+
+
+const Header = () => {
+    return (
+        <div className="bg-gray-700 py-8">
+            <h1 className="text-center text-3xl uppercase font-bold text-white">ToDo Application</h1>
+        </div>
+    );
+};
+
+export default Header;
